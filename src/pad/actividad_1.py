17108b8c079bb1b13a7d79249790d9d7b27c8ee6
Evermacea <144848387+Evermacea@users.noreply.github.com>
2025-03-19 03:40:14 +0000

    correción en el código

--- src/pad/actividad_1.py
+++ src/pad/actividad_1.py
@@ -1,26 +1,26 @@
 import json # Importamos la librería json
 import requests # Importamos la librería requests
-import sys # Importamos la librería os
+import sys # Importamos la librería sys
+
 class Ingestiones():
     def __init__(self):
         self.ruta_static = "src/pad/static/"
-        sys.stdout.reconfigure(encoding='utf-8') # Configuramos la salida estándar para que acepte caracteres especiales
+        sys.stdout.reconfigure(encoding='utf-8') 
 
     def leer_api(self, ruta):
         response = requests.get(ruta) # Hacemos una petición GET a la API
         return response.json() # Retornamos el contenido de la API en formato JSON
 
-    def escribir_json(self, nombre_archivo, datos): # Esta clase escribe en un archivo json, los datos extraidos desde la api con la información del anime Naruto
-        ruta_json = "{}json/{}.json".format(self.ruta_static, nombre_archivo) # Creamos el nombre del archivo con la extensión .json
+    def escribir_json(self, nombre_archivo, datos):
+        ruta_json = f"{self.ruta_static}json/{nombre_archivo}.json"
         with open(ruta_json, mode="w", encoding="utf-8") as archivo:
-            json.dump(datos, archivo)   # Guardamos los datos en un archivo JSON con el nombre especificado
+            json.dump(datos, archivo, ensure_ascii=False)
 
-
-# crear instancia de la clase
-ingestion = Ingestiones()
+ingestion = Ingestiones() # Crear instancia de la clase
 print(ingestion.ruta_static)
-datos_json = ingestion.leer_api("https://dattebayo-api.onrender.com/clans") # Llamamos a la función leer_api con la ruta de la API
-print("Datos del archivo json : ",datos_json)
 
-ingestion.escribir_json("clanes_naruto", datos_json)  # Guardamos los datos en un archivo JSON cuyo nombre es "clanes_naruto"
-print("Archivo json creado con éxito yuju! 🎉✨🤸‍♂️")
+datos_json = ingestion.leer_api("https://dattebayo-api.onrender.com/clans") # Llamamos a la función leer_api
+print("Datos del archivo json :", json.dumps(datos_json, ensure_ascii=False)) 
+
+ingestion.escribir_json("clanes_naruto", datos_json)  # Guardamos los datos en un archivo JSON
+print("Archivo json creado con éxito 🎉✨🤸‍♂️")
