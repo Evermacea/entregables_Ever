5a40249fca0cd2000b8214ba3d4f18d437efcefe
Evermacea <144848387+Evermacea@users.noreply.github.com>
2025-03-29 01:43:57 +0000

    Entrega_actividad_3

---
+++ src/pad/actividad_3.py
@@ -0,0 +1,134 @@
+# Importar librerias
+import pandas as pd
+import numpy as np
+import os
+from pad_clase import Pad_clase
+
+class Actividad_3():
+    def __init__(self):
+        self.ruta_raiz = "src/pad"
+        self.ruta_actividad_3 = "{}/actividad_3/".format(self.ruta_raiz)
+        self.ruta_guardado = os.path.join(self.ruta_actividad_3, "resultados.xlsx")
+        os.makedirs(self.ruta_actividad_3, exist_ok=True)
+        # Importar la función de la clase para extraer la base de datos de Kaggle
+        padclase = Pad_clase()          
+        dataset_path = padclase.download_dataset_zip()
+        csv_dir = padclase.extract_zip_files(dataset_path)
+        self.df = padclase.create_csv(csv_dir)
+      
+    def ejercicio_1(self):
+        """ Captura el resultado del ejercicio 1 y lo guarda en Excel. """
+        # Simulación de capturar el resultado desde el notebook
+        datos1 = {
+            'Granadilla': [20],
+            'Tomates': [50]    
+        }
+        d1 = pd.DataFrame(datos1)  # Convertimos a DataFrame
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio1.csv")
+        d1.to_csv(ruta_csv, index=False)
+
+    def ejercicio_2(self):
+        # self.df.loc[1,'resultado']=len(self.df) + 1
+        d2 =pd.DataFrame({
+        'Granadilla': [20, 49],
+        'Tomates': [50, 100]    
+        }, index =['ventas 2021','ventas 2022'])
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio2.csv")
+        d2.to_csv(ruta_csv, index=False)
+
+    def ejercicio_3(self):
+        # self.df.loc[2,'resultado']=len(self.df) + 2
+        utensilios = pd.Series(
+            ["3 unidades", "2 unidades", "4 unidades", "5 unidades"],
+            index=["Cuchara", "Tenedor", "Cuchillo", "Plato"],
+            name="Cocina"
+        )
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio3.csv")
+        utensilios.to_csv(ruta_csv, index=False)
+    
+    def ejercicio_4(self): # Este ejercicio me generó problemas por ser la base de datos tan grande, lo que hice fue dividirlo en archivos csv más pequeños para poder guardar por partes toda la base de datos
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio4.csv")
+        self.df.to_csv(ruta_csv, index=False) # Guardar el dataset completo en un CSV
+        print(f"Archivo guardado en: {ruta_csv}")
+
+        # Si el dataset tiene más de 50,000 filas, lo dividimos en partes más pequeñas
+        if self.df.shape[0] > 50000:
+            chunk_size = 25000  # Tamaño de cada fragmento
+            for i, chunk in enumerate(pd.read_csv(ruta_csv, chunksize=chunk_size)):
+                ruta_parte = os.path.join(self.ruta_actividad_3, f"ejercicio4_part{i+1}.csv")
+                chunk.to_csv(ruta_parte, index=False)
+                print(f"Guardado: {ruta_parte}")
+
+
+    def ejercicio_5(self):
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio5.csv")
+        self.df.head().to_csv(ruta_csv, index=False) # Guardar el dataset completo en un CSV    
+
+    def ejercicio_6(self):
+        self.df.info()
+        num_entradas = self.df.shape[0]
+        print(f"El DataFrame tiene {num_entradas} entradas.")
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio6.csv")
+        df_info = pd.DataFrame({"Número de entradas": [num_entradas]})
+        df_info.to_csv(ruta_csv, index=False)
+        
+    def ejercicio_7(self):
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio7.csv")
+        precio_promedio = self.df['price'].mean()
+        df_precio = pd.DataFrame({"Promedio de precio": [precio_promedio]})
+        df_precio.to_csv(ruta_csv, index=False)
+        
+    
+    def ejercicio_8(self):
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio8.csv")
+        precio_maximo = self.df['price'].max()
+        df_precio = pd.DataFrame({"El precio máximo es: ": [precio_maximo]})
+        df_precio.to_csv(ruta_csv, index=False)
+
+    def ejercicio_9(self):
+        Vinos_california = self.df[self.df['country']=='US']
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio9.csv")
+        Vinos_california.to_csv(ruta_csv, index=False)
+    
+    def ejercicio_10(self):
+        indice_mas_caro = self.df['price'].idxmax() # Índice del vino con el precio más alto
+        vino_mas_caro = self.df.loc[indice_mas_caro] # Información del vino más caro
+        # Mostrar el resultado
+        print("🍷 El vino más caro de California es:")
+        print(vino_mas_caro)
+
+        # Guardar la información en un CSV
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio10.csv")
+        vino_mas_caro.to_frame().T.to_csv(ruta_csv, index=False)
+    
+    def ejercicio_11(self):
+        Vinos_california = self.df[self.df['country']=='US']
+        uvas_mas_comunes = Vinos_california['variety'].value_counts().sort_values(ascending=False).reset_index() # Sobre la base del ejercicio 9 hago el filtro
+        uvas_mas_comunes.columns = ['Variety', 'Count']
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio11.csv")
+        uvas_mas_comunes.to_csv(ruta_csv, index=False)
+    
+    def ejercicio_12(self):
+        Vinos_california = self.df[self.df['country']=='US']
+        uvas_mas_comunes = Vinos_california['variety'].value_counts().sort_values(ascending=False).reset_index() # Sobre la base del ejercicio 9 hago el filtro
+        uvas_mas_comunes.columns = ['Variety', 'Count']
+        top10= uvas_mas_comunes.head(10)
+        ruta_csv = os.path.join(self.ruta_actividad_3, "ejercicio12.csv")
+        top10.to_csv(ruta_csv, index=False)
+
+    def ejecutar(self): # Función para ejecutar todos los ejercicios
+        self.ejercicio_1()
+        self.ejercicio_2()
+        self.ejercicio_3()
+        self.ejercicio_4()
+        self.ejercicio_5()
+        self.ejercicio_6()
+        self.ejercicio_7()
+        self.ejercicio_8()
+        self.ejercicio_9()
+        self.ejercicio_10()
+        self.ejercicio_11()
+        self.ejercicio_12()
+
+act3 = Actividad_3() # Instnancia de la clase
+act3.ejecutar() # Activar o llamar la función que ejcuta todos los puntos
---
+++ src/pad/pad_clase.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import kagglehub
+import os
+import zipfile
+import matplotlib
+import plotly.express as px
+matplotlib.use('Agg') 
+import matplotlib.pyplot as plt
+import tkinter
+import sys # Importamos la libreria sys
+# tkinter._test()
+import numpy as np
+
+
+class Pad_clase:
+    def __init__(self):
+        pass
+        # sys.stdout.reconfigure(encoding='utf-8') 
+        
+    def download_dataset_zip(self):
+        print("Descargando dataset desde Kaggle...")
+        dataset_path = kagglehub.dataset_download("zynicide/wine-reviews")
+        print("Ruta al dataset:", dataset_path)
+        return dataset_path
+    
+    def extract_zip_files(self,dataset_path):
+        zip_files = [f for f in os.listdir(dataset_path) if f.endswith('.zip')]
+        if zip_files:
+            zip_file = os.path.join(dataset_path, zip_files[0])
+            extract_dir = os.path.join(dataset_path, "extracted")
+            os.makedirs(extract_dir, exist_ok=True)
+            print(f"Extrayendo {zip_file} en {extract_dir}...")
+            with zipfile.ZipFile(zip_file, "r") as z:
+                z.extractall(extract_dir)
+            return extract_dir
+        else:
+            # Si no se encuentra un ZIP, se verifica si existen archivos CSV en la ruta
+            csv_files = [f for f in os.listdir(dataset_path) if f.endswith('.csv')]
+            if csv_files:
+                print("No se encontró archivo ZIP pero se detectaron archivos CSV; se asume que el dataset ya se encuentra extraído.")
+                return dataset_path
+            else:
+                raise FileNotFoundError("No se encontró ningún archivo .zip ni archivos .csv en la ruta del dataset")
+    
+
+    def create_csv(self, csv_dir):
+        os.makedirs('src/static/csv', exist_ok=True)
+
+        csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
+
+        if not csv_files:
+            raise FileNotFoundError("No se encontraron archivos CSV en el directorio extraído")
+
+        df_list = []  # Lista para almacenar los DataFrames
+
+        for file in csv_files:
+            file_path = os.path.join(csv_dir, file)
+            print(f"Leyendo {file_path}...")
+
+            try:
+                df = pd.read_csv(file_path, encoding="latin1")
+                df_list.append(df)  # Guardamos el DataFrame en la lista
+                print(f"✅ CSV cargado correctamente: {file}")
+            except Exception as e:
+                print(f"❌ Error al leer {file}: {e}")
+
+        if not df_list:  # Si no se logró leer ningún archivo
+            raise ValueError("No se pudo cargar ningún archivo CSV correctamente")
+
+        df_final = pd.concat(df_list, ignore_index=True)  # Unimos todos los CSVs en uno solo
+        print("✅ CSVs combinados correctamente")
+
+        return df_final  # Retornamos el DataFrame consolidado
+
+    
+    def grafico_normal(self,df):
+        cantidad = len(df)
+        # cantidad = 209306
+        np.random.seed(0)
+        generados = np.random.randn(cantidad)
+        serie = pd.Series(generados)
+        df["random_g"] = serie
+        df.to_csv("dataset_nuevo.csv")
+        #plt.figure()
+        df["num_units"].plot(kind = 'barh')
+        #print(df["random_g"].describe())
+        #plt.show()
+        plt.savefig("grafico_normal_2_line.png")
+        #plt.legend(loc='datos aleatorios')
+    
+    def grafico_df_xy(self,df):
+        if "random_g" not in df.columns:
+            np.random.seed(0)
+            df["random_g"] = np.random.randn(len(df))
+        
+        # Definir las columnas que se quieren graficar
+        columnas = ["crash_month", "random_g"]
+        
+        # (Opcional) Si deseas reiniciar el índice de df:
+        df.reset_index(drop=True, inplace=True)
+        
+        # Seleccionar las columnas y graficar
+        df1 = df[columnas]
+        df1.plot()
+        plt.xlabel("Índice")
+        plt.title("Gráfico de crash_month y random_g")
+        plt.savefig("grafico_normal_3.png")
+        plt.show()
+    
+    def graficos_agrupados(self,df = pd.DataFrame(), lista_columnas=[]):
+        for col in lista_columnas:
+            conteo = df[col].value_counts().sort_index()
+            plt.figure()
+            conteo.plot(kind= "bar")
+            plt.xlabel(col)
+            plt.ylabel("cantidad")
+            plt.show()
+
+# padclase = Pad_clase()          
+# dataset_path = padclase.download_dataset_zip()
+# csv_dir = padclase.extract_zip_files(dataset_path)
+# df = padclase.create_csv(csv_dir)
+# print(df.describe(),df.count,df.info())
+# df.to_csv("dataset_kaggle.csv")
+# padclase.grafico_normal(df)
+# padclase.grafico_df_xy(df)
+#209306
+#print(df.describe(),df.count,df.info())
